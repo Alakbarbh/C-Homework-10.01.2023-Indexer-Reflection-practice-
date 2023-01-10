96391d067150c85daa466580adc55dcc96d93294
Alakbar Hasanov <alakbarbh@code.edu.az>
2023-01-11 01:13:40 +0400

    C#Homework 10.01.2023 (Indexer,Reflection pracitice)

---
+++ Homework 10.01.2023(Practice)/Controllers/AccountContreller.cs
@@ -0,0 +1,35 @@
+﻿using ServiceLayer.Services;
+using ServiceLayer.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_10._01._2023_Practice_.Controllers
+{
+    public class AccountContreller
+    {
+        private readonly IAccountService _service;
+
+        public AccountContreller()
+        {
+            _service = new AccountService();
+        }
+
+        public void Register()
+        {
+            Console.WriteLine("Add your username");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Add your email");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("Add your password");
+            string password = Console.ReadLine();
+
+            var result = _service.Register(username, email, password);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ServiceLayer/Services/AccountService.cs
@@ -0,0 +1,42 @@
+﻿using ServiceLayer.Exceptions;
+using ServiceLayer.Helpers.Constants;
+using ServiceLayer.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Services
+{
+    public class AccountService : IAccountService
+    {
+        public string Register(string username, string email, string password)
+        {
+            try
+            {
+                var result = GetAllEmails();
+
+                if (result.Contains(email))
+                {
+                    throw new RegisterExceptions(ResponseMessages.InValidRegisterMessage);
+                }
+
+                return ResponseMessages.SuccessRegisterMessage;
+
+            }
+            catch (Exception ex)
+            {
+
+                return ex.Message;
+            }
+
+       
+        }
+
+        private List<string> GetAllEmails()
+        {
+            return new List<string>() { "pervin12@gmail.com", "ali@code.edu.az", "agshin@gamil.com" };
+        }
+    }
+}
